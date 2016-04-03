17cb1380fba20bf976cfc1ed156d699b0362d62c
IvanEh <ivanehreshi@gmail.com>
2016-04-03 17:32:01 +0300

    Server divides task into subtasks

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -4,15 +4,29 @@ api.net = require('net');
 
 var socket = new api.net.Socket();
 var user;
-var port = 2010;
+var port = 2016;
 
 socket.connect({
   port: port,
   host: '127.0.0.1',
 }, function() {
+  socket.write('"free"');
+
   socket.on('data', function(data) {
-    task = JSON.parse(data);
-    task = task.map((x) => 2*x);
-    socket.write(JSON.stringify(task));
+    setTimeout(() => {
+      var task = JSON.parse(data);
+      if(typeof data !== 'object') {
+        return;
+      }
+      var output = task.input.map((x) => 2*x);
+      var result = new Result(task.id, output);
+      socket.write(JSON.stringify(result));
+      socket.write('"free"');
+    }, 4000);
   });
 });
+
+function Result(id, result) {
+  this.id = id;
+  this.values = result;
+}
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -1,22 +1,69 @@
 var api = {};
 global.api = api;
 api.net = require('net');
-var port = 2010;
+var port = 2016;
 var user = {name: 'Marcus Aurelius'}
 var task = [1, 2, 3, 4, 5, 6];
+var availableClients = 3;
+var nextSubtask = 0;
+var subtasks = initSubtasks(task, availableClients);
+var results = [];
+
 
 var server = api.net.createServer(function(socket) {
-  console.log('Connected: ' + socket.localAddress);
-  socket.write(JSON.stringify(task));
+  console.dir(socket.address());
   socket.on('data', function(data) {
-    var result = JSON.parse(data);
-    console.log('result ' + result);
+    var msg = JSON.parse(data);
+
+    console.dir(msg);
+    if(msg == 'free' && nextSubtask < subtasks.length) {
+      sendSubtask(socket, subtasks[nextSubtask]);
+      ++nextSubtask;
+    }else {
+      results[msg.id] = msg;
+    }
+
+    if(results.length == subtasks.length) {
+      var result = joinResults(results);
+      console.log(result);
+    }
+
   });
 }).listen(port);
 
 function publishTask(socket, task) {
   socket.write(JSON.stringify(task));
 //socket.end('')
+}
+function sendSubtask(socket, subtask) {
+  var json = JSON.stringify(subtask);
+  socket.write(json);
+}
+
+function joinResults() {
+  var result = [];
+  results.forEach((res) => {
+    res.values.forEach((x) => {
+      result.push(x);
+    })
+  });
+  return result;
+}
+
+function Subtask(id, input) {
+  this.completed = false;
+  this.input = input;
+  this.id = id;
+}
+
+function initSubtasks(task, parts) {
+  var subtasks = [];
+  var partSize = task.length / parts;
+  for(var i = 1; i <= parts-1; i++) {
+    subtasks.push(new Subtask(i-1, task.slice((i-1)*partSize, (i-1)*partSize + partSize)))
+  }
+  subtasks.push(new Subtask(parts-1, task.slice((parts - 1)*partSize)));
+  return subtasks;
 }
 
 setInterval(() => server.unref(), 4000);
